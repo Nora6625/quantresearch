b71355de1d4c2661beed07db9a714689a6dd8805
Noragkm <165533306+Noragkm@users.noreply.github.com>
2024-04-13 11:53:57 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -8,26 +8,22 @@ Created on Sat Apr  4 07:37:00 2020
 import pandas as pd
 import numpy as np
 
+# windows
 # dir_main = "C:\\Users\\10164\\Documents\\WeChat Files\\wxid_plvhta31cu2i22\\FileStorage\\File\\2024-03\\量化编程\\量化编程\\"
+
 # dir_result = dir_main + "result\\"
 # dir_code = dir_main + "code\\"
-# dir_data = dir_main + "data\\"
-# dir_data_raw = dir_data + "raw\\"
+
 # dir_data_raw_market = dir_main + "market\\"
 # dir_data_final = dir_main + "final\\"
 # dir_data_raw_account = dir_main + "account\\"
 
-# dir_data_final = dir_data + "final\\"
-# dir_data_raw_account = dir_data_raw + "account\\"
-# dir_data_raw_market = dir_data_raw + "market\\"
+# Mac
+dir_main = r"/Users/guokaimeng/Desktop/郭铠萌量化/"
 
-dir_main = r"/Users/guokaimeng/Desktop/量化编程/"
 dir_result = dir_main + "result/"
 dir_code = dir_main + "code/"
-dir_data = dir_main + "data/"
-dir_data_raw = dir_data + "raw/"
+
 dir_data_raw_market = dir_main + "market/"
 dir_data_final = dir_main + "final/"
 dir_data_raw_account = dir_main + "account/"
-
-dir_result = dir_main + "result/"
